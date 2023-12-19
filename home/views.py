2cac9c24366244e372834ea0149850bd50c81126
Harikrishnan NJ <harineduvely2015@gmail.com>
2023-12-19 12:34:27 +0530

    second commit

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView,ListAPIView
+from rest_framework.generics import ListCreateAPIView,ListAPIView,RetrieveUpdateDestroyAPIView
 from . models import Items
 from . serilaizers import ItemSerializer
 # Create your views here.
 class ItemListCreateView(ListCreateAPIView):
+    queryset = Items.objects.all()
+    serializer_class = ItemSerializer
+    
+class ItemDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Items.objects.all()
     serializer_class = ItemSerializer
